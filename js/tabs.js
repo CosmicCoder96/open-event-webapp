ffe0eb53e8a11400cc4b3b9de147b6e069d69ea9
shubham-padia <shubhamapadia@gmail.com>
2017-01-11 07:05:38 +0000

    rebuild pages at 32c17e6

--- js/tabs.js
+++ js/tabs.js
@@ -1,6 +1,8 @@
+
 (function () {
   'use strict';
   var tabs = function(options) {
+    
     var i;
     var el = document.querySelector(options.el);
     var tabNavigationLinks = el.querySelectorAll(options.tabNavigationLinks);
@@ -9,36 +11,36 @@
     var initCalled = false;
 
     var goToTab = function(index) {
-        if (index !== activeIndex && index >= 0 && index <= tabNavigationLinks.length) {
-            tabNavigationLinks[activeIndex].classList.remove('is-active');
-            tabNavigationLinks[index].classList.add('is-active');
-            tabContentContainers[activeIndex].classList.remove('is-active');
-            tabContentContainers[index].classList.add('is-active');
-            activeIndex = index;
-        }
+      if (index !== activeIndex && index >= 0 && index <= tabNavigationLinks.length) {
+        tabNavigationLinks[activeIndex].classList.remove('is-active');
+        tabNavigationLinks[index].classList.add('is-active');
+        tabContentContainers[activeIndex].classList.remove('is-active');
+        tabContentContainers[index].classList.add('is-active');
+        activeIndex = index;
+      }
     };
     var handleClick = function(link, index) {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            goToTab(index);
-        });
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+        goToTab(index);
+      });
     };
     var init = function() {
-        var link;
+      var link;
 
-        if (!initCalled) {
-            initCalled = true;
-            el.classList.remove('no-js');
-            for (i = 0; i < tabNavigationLinks.length; i++) {
-                link = tabNavigationLinks[i];
-                handleClick(link, i);
-            }
+      if (!initCalled) {
+        initCalled = true;
+        el.classList.remove('no-js');
+        for (i = 0; i < tabNavigationLinks.length; i++) {
+          link = tabNavigationLinks[i];
+          handleClick(link, i);
         }
+      }
     };
 
     return {
-        init: init,
-        goToTab: goToTab
+      init: init,
+      goToTab: goToTab
     };
 
   };
