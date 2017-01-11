e281bc100211f3f463ec12ce69bb7b2b2bc26951
shubham-padia <shubhamapadia@gmail.com>
2017-01-11 00:05:13 +0000

    rebuild pages at 9142075

--- js/tabs.js
+++ js/tabs.js
@@ -1,51 +1,64 @@
 (function() {
-  "use strict";
-  var tabs = function(options) {
-    var i, link;
-    var el = document.querySelector(options.el);
-    var tabNavigationLinks = el.querySelectorAll(options.tabNavigationLinks);
-    var tabContentContainers = el.querySelectorAll(options.tabContentContainers);
-    var activeIndex = 0;
-    var initCalled = false;
-    var goToTab = function(index) {
-      if (index !== activeIndex && index >= 0 && index <= tabNavigationLinks.length) {
-        tabNavigationLinks[activeIndex].classList.remove('is-active');
-        tabNavigationLinks[index].classList.add('is-active');
-        tabContentContainers[activeIndex].classList.remove('is-active');
-        tabContentContainers[index].classList.add('is-active');
-        activeIndex = index;
-      }
-    };
-    var handleClick = function(link, index) {
-      link.addEventListener('click', function(e) {
-        e.preventDefault();
-        goToTab(index);
-      });
-    };
-    return {
-      init: init,
-      goToTab: goToTab
-    };
-    var init = function() {
-      if (!initCalled) {
-        initCalled = true;
-        el.classList.remove('no-js');
 
-        for (i = 0; i < tabNavigationLinks.length; i++) {
-          link = tabNavigationLinks[i];
-          handleClick(link, i);
-        }
-      }
-    };
-  };
+    'use strict';
 
-  window.tabs = tabs;
+    var tabs = function(options) {
+
+        var el = document.querySelector(options.el);
+        var tabNavigationLinks = el.querySelectorAll(options.tabNavigationLinks);
+        var tabContentContainers = el.querySelectorAll(options.tabContentContainers);
+        var activeIndex = 0;
+        var initCalled = false;
+
+        var goToTab = function(index) {
+            if (index !== activeIndex && index >= 0 && index <= tabNavigationLinks.length) {
+                tabNavigationLinks[activeIndex].classList.remove('is-active');
+                tabNavigationLinks[index].classList.add('is-active');
+                tabContentContainers[activeIndex].classList.remove('is-active');
+                tabContentContainers[index].classList.add('is-active');
+                activeIndex = index;
+            }
+        };
+        var handleClick = function(link, index) {
+            link.addEventListener('click', function(e) {
+                e.preventDefault();
+                goToTab(index);
+            });
+        };
+        var init = function() {
+            if (!initCalled) {
+                initCalled = true;
+                el.classList.remove('no-js');
+
+                for (i = 0; i < tabNavigationLinks.length; i++) {
+                    link = tabNavigationLinks[i];
+                    handleClick(link, i);
+                }
+            }
+        };
+
+
+
+
+        return {
+            init: init,
+            goToTab: goToTab
+        };
+
+    };
+
+
+    window.tabs = tabs;
+
 })();
-    var myTabs = tabs({
-         el: '#tabs',
-         tabNavigationLinks: '.tabs-nav__link',
-         tabContentContainers: '.tab'
-    });
+
+
+var myTabs = tabs({
+    el: '#tabs',
+    tabNavigationLinks: '.tabs-nav__link',
+    tabContentContainers: '.tab'
+
+});
 document.getElementsByClassName('tabs-nav__link')[0].className += ' is-active';
 document.getElementsByClassName('tab')[0].className += ' is-active';
 myTabs.init();
